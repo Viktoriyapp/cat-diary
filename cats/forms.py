e89d62e187e83604ff049c3b46a5eeb66cec7953
Viktoriyapp <todorova.p.viktoria@gmail.com>
2026-02-06 23:25:33 +0200

    Added Cat ModelForm

---
+++ cats/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Cat
+
+
+class CatForm(forms.ModelForm):
+    class Meta:
+        model = Cat
+        fields = ['name', 'birth_date', 'personality', 'photo']
+
+        labels = {
+            'name': 'My name',
+            'birth_date': 'My birthday',
+            'personality': 'My personality',
+            'photo': 'My photo',
+        }
+
+        help_texts = {
+            'name': 'What do humans usually call me?',
+            'birth_date': 'If you remember it... naps can affect memory...',
+            'personality': 'How would you describe yourself?',
+            'photo': 'A majestic photo of me. Optional, but recommended.',
+        }
