85128245749dd8778506b7f79152e6bbc81efb43
Viktoriyapp <todorova.p.viktoria@gmail.com>
2026-02-21 01:14:21 +0200

    Handled static files and media

--- cat_diary/urls.py
+++ cat_diary/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -29,3 +31,7 @@ urlpatterns = [
 ]
 
 handler404 = 'common.views.custom_404'
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
