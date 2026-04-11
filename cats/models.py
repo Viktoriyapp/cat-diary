d9aa65fb6b2b90d48993bc091e457f3ab8c0b95a
Viktoriyapp <todorova.p.viktoria@gmail.com>
2026-04-11 19:56:59 +0300

    Extended User with cat profile using OneToOne relationship

--- cats/models.py
+++ cats/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -18,6 +19,12 @@ class Cat(models.Model):
     personality = models.CharField(max_length=30, choices=PersonalityChoices.choices, default=PersonalityChoices.CALM,)
     photo = models.ImageField(upload_to='cats/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.name
