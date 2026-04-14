cbb8d4bfb93743a2ce665e4fbd9d5b61f1a6e579
Viktoriyapp <todorova.p.viktoria@gmail.com>
2026-04-14 21:42:36 +0300

    chore:add API endpoint for personal mood entries

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from api.views import MoodEntryApiListView
+from api.views import MoodEntryApiListView, MyMoodEntryApiListView
 
 app_name = 'api'
 
 urlpatterns = [
     path('moods/', MoodEntryApiListView.as_view(), name='mood-list'),
+path('my-moods/', MyMoodEntryApiListView.as_view(), name='my-mood-list'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,9 +2,21 @@ from rest_framework.generics import ListAPIView
 from api.permissions import CanViewAllMoodsPermission
 from api.serializers import MoodEntrySerializer
 from moods.models import MoodEntry
+from rest_framework.permissions import IsAuthenticated
 
 
 class MoodEntryApiListView(ListAPIView):
     queryset = MoodEntry.objects.all().order_by('-date')
     serializer_class = MoodEntrySerializer
     permission_classes = [CanViewAllMoodsPermission]
+
+
+class MyMoodEntryApiListView(ListAPIView):
+    serializer_class = MoodEntrySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if not hasattr(self.request.user, 'cat'):
+            return MoodEntry.objects.none()
+
+        return MoodEntry.objects.filter(cat=self.request.user.cat).order_by('-date')
