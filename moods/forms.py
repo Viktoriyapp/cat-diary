0614bb9e3ca305a443d9a3484a54a9224a9cef06
Viktoriyapp <todorova.p.viktoria@gmail.com>
2026-04-15 15:20:36 +0300

    chore:added toys field to mood entry form

--- moods/forms.py
+++ moods/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from activities.models import Activity
+from cats.models import Toy
 from common.choices import EnergyChoices
 from moods.models import MoodEntry, DayTag
 
@@ -7,8 +8,7 @@ from moods.models import MoodEntry, DayTag
 class MoodEntryForm(forms.ModelForm):
     class Meta:
         model = MoodEntry
-        fields = ['date', 'mood', 'energy_level', 'activities', 'personal_note', 'day_tags']
-
+        fields = ['date', 'mood', 'energy_level', 'activities', 'day_tags', 'personal_note', 'toys']
         labels = {
             'date': 'Today`s date',
             'mood': 'How am I today?',
@@ -16,12 +16,14 @@ class MoodEntryForm(forms.ModelForm):
             'activities': 'What did I do today?',
             'personal_note': 'My personal diary note',
             'day_tags': 'How would I describe this day?',
+            'toys': 'Which toys did I play with?'
         }
 
         help_texts = {
             'activities': 'Mark the activities you tackled today',
             'personal_note': 'Optional... but do share your secret thoughts',
-            'day_tags': 'Optional tags that describe the vibe of the day.'
+            'day_tags': 'Optional tags that describe the vibe of the day.',
+            'toys': 'Optional toys you used during the day.',
         }
 
         widgets = {
@@ -34,13 +36,15 @@ class MoodEntryForm(forms.ModelForm):
                 'rows': 3,
                 'class': 'form-control',
             }),
-            'day_tags': forms.SelectMultiple(attrs={'class': 'form-select'})
+            'day_tags': forms.SelectMultiple(attrs={'class': 'form-select'}),
+            'toys': forms.SelectMultiple(attrs={'class': 'form-select'}),
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['activities'].queryset = Activity.objects.order_by('name')
         self.fields['day_tags'].queryset = DayTag.objects.order_by('name')
+        self.fields['toys'].queryset = Toy.objects.none() # will say who the user is from view
 
     def clean_personal_note(self):
         note = self.cleaned_data.get('personal_note')
