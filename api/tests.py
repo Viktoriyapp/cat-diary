6bb13710f2e809326c7243e29024d98fc8f44506
Viktoriyapp <todorova.p.viktoria@gmail.com>
2026-04-17 00:39:41 +0300

    chore: added API tests for personal and moderator mood endpoints

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,52 @@
+from django.contrib.auth.models import User, Permission
 from django.test import TestCase
+from django.urls import reverse
+from cats.models import Cat
+from moods.models import MoodEntry
 
-# Create your tests here.
+
+class MoodApiTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='user1', password='12345')
+        self.cat = Cat.objects.create(user=self.user, name='Tom')
+
+        self.other_user = User.objects.create_user(username='user2', password='12345')
+        self.other_cat = Cat.objects.create(user=self.other_user, name='Jerry')
+
+        self.entry = MoodEntry.objects.create(
+            cat=self.cat,
+            date='2026-04-10',
+            mood='happy',
+            energy_level=2,
+        )
+        self.other_entry = MoodEntry.objects.create(
+            cat=self.other_cat,
+            date='2026-04-11',
+            mood='sad',
+            energy_level=1,
+        )
+
+    def test_anonymous_user_cannot_access_my_moods_api(self):
+        response = self.client.get(reverse('api:my-mood-list'))
+        self.assertEqual(response.status_code, 403)
+
+    def test_logged_user_sees_only_own_entries_in_my_moods_api(self):
+        self.client.login(username='user1', password='12345')
+        response = self.client.get(reverse('api:my-mood-list'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '2026-04-10')
+        self.assertNotContains(response, '2026-04-11')
+
+    def test_moderator_can_access_all_moods_api(self):
+        moderator = User.objects.create_user(username='mod', password='12345')
+        permission = Permission.objects.get(codename='can_view_all_moods')
+        moderator.user_permissions.add(permission)
+
+        self.client.login(username='mod', password='12345')
+        response = self.client.get(reverse('api:mood-list'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '2026-04-10')
+        self.assertContains(response, '2026-04-11')
--- moods/tests.py
+++ moods/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 
 from moods.forms import MoodEntryForm
 from moods.models import MoodEntry
@@ -95,3 +95,67 @@ class MoodEntryFormTests(TestCase):
             user=self.user)
 
         self.assertTrue(form.is_valid())
+
+
+from django.urls import reverse
+
+
+class MoodEntryViewTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='user1', password='12345')
+        self.cat = Cat.objects.create(user=self.user, name='Tom')
+
+        self.other_user = User.objects.create_user(username='user2', password='12345')
+        self.other_cat = Cat.objects.create(user=self.other_user, name='Jerry')
+
+        self.entry = MoodEntry.objects.create(
+            cat=self.cat,
+            date='2026-04-10',
+            mood='happy',
+            energy_level=2)
+        self.other_entry = MoodEntry.objects.create(
+            cat=self.other_cat,
+            date='2026-04-11',
+            mood='sad',
+            energy_level=1,
+        )
+
+    def test_login_required_for_list(self):
+        response = self.client.get(reverse('moods:list'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_sees_only_own_entries(self):
+        self.client.login(username='user1', password='12345')
+        response = self.client.get(reverse('moods:list'))
+
+        self.assertContains(response, f'/moods/{self.entry.pk}/')
+        self.assertNotContains(response, f'/moods/{self.other_entry.pk}/')
+
+    def test_user_cannot_access_other_entry_detail(self):
+        self.client.login(username='user1', password='12345')
+
+        response = self.client.get(
+            reverse('moods:detail', args=[self.other_entry.pk])
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_user_can_access_own_entry_detail(self):
+        self.client.login(username='user1', password='12345')
+
+        response = self.client.get(
+            reverse('moods:detail', args=[self.entry.pk])
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_moderator_can_access_all_entries(self):
+        moderator = User.objects.create_user(username='mod', password='12345')
+        moderator.is_staff = True
+        moderator.save()
+
+        permission = Permission.objects.get(codename='can_view_all_moods')
+        moderator.user_permissions.add(permission)
+
+        self.client.login(username='mod', password='12345')
+        response = self.client.get(reverse('moods:all'))
+        self.assertEqual(response.status_code, 200)
