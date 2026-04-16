56efc3b264cfd7c4260b877f9a02296e4ab4b169
Viktoriyapp <todorova.p.viktoria@gmail.com>
2026-04-16 18:53:42 +0300

    fix: render register form labels and help texts from form fields

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,23 +2,34 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User, Group
 from django.db import transaction
-
+from cats.forms import CatForm
 from cats.models import Cat
 
 
 class CatUserRegisterForm(UserCreationForm):
-    name = forms.CharField(max_length=30, label='Cat Name')
-    birth_date = forms.DateField(
-        required=False,
-        widget=forms.DateInput(attrs={'type': 'date'}),
-        label='Birth Date',
-    )
-    personality = forms.ChoiceField(choices=Cat.PersonalityChoices.choices, label='Personality')
-    photo = forms.ImageField(required=False, label='Profile photo')
+    name = forms.CharField(max_length=30)
+    birth_date = forms.DateField(required=False,)
+    personality = forms.ChoiceField(choices=Cat.PersonalityChoices.choices,)
+    photo = forms.ImageField(required=False)
 
     class Meta:
         model = User
         fields = ('username',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        cat_form = CatForm()
+
+        for field_name in ['name', 'birth_date', 'personality', 'photo']:
+            if field_name in self.fields and field_name in cat_form.fields:
+                self.fields[field_name].label = cat_form.fields[field_name].label
+                self.fields[field_name].help_text = cat_form.fields[field_name].help_text
+                self.fields[field_name].widget = cat_form.fields[field_name].widget
+
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
 
     @transaction.atomic
     def save(self, commit=True):
